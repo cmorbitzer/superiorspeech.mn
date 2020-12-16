42e946c6a40bffc2bc129c2089c1eaf44ce65570
Chris Morbitzer <chris.morbitzer@gmail.com>
2020-12-16 13:35:43 -0600

    Add footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 export default ({ children }: React.PropsWithChildren<{}>) => (
   <>
@@ -8,5 +9,7 @@ export default ({ children }: React.PropsWithChildren<{}>) => (
     <main className="mt-16 sm:mt-24 lg:mt-32 pb-16 sm:pb-24 lg:pb-32">
       {children}
     </main>
+
+    <Footer />
   </>
 );
