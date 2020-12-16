b651cbd45d50febf674172d5db7ae797292830af
Chris Morbitzer <chris.morbitzer@gmail.com>
2020-12-16 17:19:50 -0600

    Update 404.tsx

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+
+export default () => (
+  <Layout>
+    <SEO title="404: Not Found" />
+    <h1>404: Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+  </Layout>
+);
